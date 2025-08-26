9d7a973fdcf0fb4607f4a6c93fef84a1c6afcf0b
mrl <mrl@contraste.com>
2025-08-26 16:29:59 +0200

    creation footer + correction hero section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,100 +1,69 @@
 import React from 'react';
-import './Footer.css';
-import { Button } from '../Button/Button';
+import './Footer.scss';
 import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
+    <footer className='footer-container'>
+      <div className='footer-content'>
+        {/* Section principale avec logo et description */}
+        <div className='footer-main'>
+          <div className='footer-logo'>
+            <img src="/images/logo.png" alt="Badminton Club" />
+            <span>Badminton Club</span>
           </div>
-          <div className='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+          <p>Rejoignez notre club et découvrez la passion du badminton dans une ambiance conviviale.</p>
+        </div>
+
+        {/* Section liens rapides */}
+        <div className='footer-section'>
+          <h4>Liens rapides</h4>
+          <div className='links-grid'>
+            <Link to='/'>Accueil</Link>
+            <Link to='/a-propos'>À propos</Link>
+            <Link to='/horaires'>Horaires</Link>
+            <Link to='/evenements'>Événements</Link>
           </div>
         </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
+
+        {/* Section contact et horaires combinées */}
+        <div className='footer-section'>
+          <h4>Contact & Horaires</h4>
+          <div className='contact-compact'>
+            <div className='contact-item'>
+              <i className='fas fa-phone'></i>
+              <a href='tel:+33123456789'>03 20 12 34 56</a>
+            </div>
+            <div className='contact-item'>
+              <i className='fas fa-envelope'></i>
+              <a href='mailto:contact@badminton-club.be'>contact@badminton-club.be</a>
+            </div>
+            <div className='contact-item'>
+              <i className='fas fa-clock'></i>
+              <span>Lun-Ven: 18h-22h | Sam: 14h-18h</span>
+            </div>
           </div>
         </div>
       </div>
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <Link to='/' className='social-logo'>
-              TRVL
-              <i className='fab fa-typo3' />
-            </Link>
-          </div>
-          <small className='website-rights'>TRVL © 2020</small>
+
+      {/* Footer bottom simplifié */}
+      <div className='footer-bottom'>
+        <div className='footer-bottom-content'>
+          <span>&copy; 2025 Badminton Club</span>
           <div className='social-icons'>
-            <Link
-              className='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i className='fab fa-facebook-f' />
-            </Link>
-            <Link
-              className='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i className='fab fa-instagram' />
-            </Link>
-            <Link
-              className='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i className='fab fa-youtube' />
-            </Link>
-            <Link
-              className='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i className='fab fa-twitter' />
-            </Link>
-            <Link
-              className='social-icon-link linkedin'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i className='fab fa-linkedin' />
-            </Link>
+            <a href='https://facebook.com' target='_blank' rel='noopener noreferrer' aria-label='Facebook'>
+              <i className='fab fa-facebook-f'></i>
+            </a>
+            <a href='https://instagram.com' target='_blank' rel='noopener noreferrer' aria-label='Instagram'>
+              <i className='fab fa-instagram'></i>
+            </a>
+            <a href='mailto:contact@badminton-club.be' aria-label='Email'>
+              <i className='fas fa-envelope'></i>
+            </a>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </footer>
   );
 }
 
